ff2003e5ae639e14f5155704d7850b9927366a3a
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-08 18:48:16 +0300

    add commenting
    commented code in index, styling and script pages for explanation purposes

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
     $("form").submit(function(event) {
 
-        event.preventDefault();
-        $("#list").empty();
+        event.preventDefault(); //prevent oage reload
+        $("#list").empty(); //empty results list before beginning
 
-        var userInput = parseInt($("#input").val());
+        var userInput = parseInt($("#input").val()); //set user input to variable userInput
 
+        // loop as many times as userInput, checking if current number fits any of the conditions and give corresponding output
         for (var count = 1; count <= userInput; count++) {
-            if (count % 3 === 0) {
-                if (count % 5 === 0) {
+            if (count % 3 === 0) { //check if current number is divisible by 3
+                if (count % 5 === 0) { //check if current number is also divisible by 5; if so, it is divisible by 15 and therefore output is 'pingpong'
                     $('ul#list').append('<li>pingpong</li>');
 
-                } else {
+                } else { //if not divisible by 5, output 'ping' for 3
                     $('ul#list').append('<li>ping</li>');
                 }
 
-            } else if (count % 5 === 0) {
+            } else if (count % 5 === 0) {//check if current number is divisible by 5; if so, output 'pong'
                 $('ul#list').append('<li>pong</li>');
-            } else {
+            } else {//if current number is divisible by none, output current number
                 $('ul#list').append('<li>' + count + '</li>');
             }
         }
-        $("#results").show();
+        $("#results").show(); //show results window with list data
     });
 });
